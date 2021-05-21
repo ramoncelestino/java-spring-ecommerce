8a862f82789ce2e0a0f1ed64b3088751eb580207
Ramon Celestino <ramoncelesteramos@gmail.com>
2021-05-21 16:45:45 -0300

    First commit

---
+++ src/main/java/com/ramoncelestino/cursosb/CursosbApplication.java
@@ -0,0 +1,13 @@
+package com.ramoncelestino.cursosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosbApplication.class, args);
+	}
+
+}
